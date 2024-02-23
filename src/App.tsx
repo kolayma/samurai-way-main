5d779e9e3fd085568e520054eaebf35709d29471
kolayma <kolayma@gmail.com>
2024-02-23 11:45:40 +1100

    added new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+     <Header />
+     <Technologies />
+     <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = ()=> {
+    return (
+        <div>
+            IT-Kamasutra.com footer
+        </div>
+    )
+}
+export default Footer;
