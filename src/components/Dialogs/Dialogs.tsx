d34eaad43aa19ac7bbb72ed4a844dea4dcd7ce95
kolayma <kolayma@gmail.com>
2024-03-25 19:35:00 +1100

    added divs to dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import s from './Dialogs.module.css'
 
 const Dialogs = ()=> {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Kristy
+                </div>
+                <div className={s.dialog}>
+                    Katya
+                </div>
+                <div className={s.dialog}>
+                    Serega
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello!</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Where are you from?</div>
+            </div>
+
+
         </div>
     )
 }
